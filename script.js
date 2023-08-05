868fe70f9e5fa067132852478720e56a9e7cd7d9
Pragati Prakash <88521810+pragati6412@users.noreply.github.com>
2023-08-05 07:33:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var randomNumber1=Math.random();
+randomNumber1=(Math.floor(randomNumber1*6))+1;
+
+var str1="/images/dice"+String(randomNumber1)+".png";
+document.getElementById("img1").src=str1;
+
+var randomNumber2=Math.random();
+randomNumber2=(Math.floor(randomNumber2*6))+1;
+
+var str2="/images/dice"+String(randomNumber2)+".png";
+document.getElementById("img2").src=str2;
+
+var sel=document.querySelector("div h1");
+
+if (randomNumber1>randomNumber2){
+    sel.innerHTML="Player 1 Wins!";
+    sel.classList.add("p1Win")
+}
+else if (randomNumber1===randomNumber2){
+    sel.innerHTML="Draw!!";
+}
+else{
+    sel.innerHTML="Player 2 Wins";
+    sel.classList.add("p2Win");
+}
