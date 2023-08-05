9db6b8152c9a784e5fd95fdb0f0d44ea41452290
Pragati Prakash <88521810+pragati6412@users.noreply.github.com>
2023-08-05 07:54:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var randomNumber1=Math.random();
 randomNumber1=(Math.floor(randomNumber1*6))+1;
 
-var str1="/dice"+String(randomNumber1)+".png";
+var str1="dice"+String(randomNumber1)+".png";
 document.getElementById("img1").src=str1;
 
 var randomNumber2=Math.random();
 randomNumber2=(Math.floor(randomNumber2*6))+1;
 
-var str2="/dice"+String(randomNumber2)+".png";
+var str2="dice"+String(randomNumber2)+".png";
 document.getElementById("img2").src=str2;
 
 var sel=document.querySelector("div h1");
